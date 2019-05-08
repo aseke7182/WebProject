7178142cd0ea618e6812b5b49e4019bc3f316018
leila <leiladoskumbay@gmail.com>
2019-05-08 16:20:51 +0600

    new url

--- plantyouBack/api/urls.py
+++ plantyouBack/api/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('catalogs/', views.CatalogList.as_view()),
     path('catalogs/<int:pk>/', views.CatalogInfo.as_view()),
     path('catalogs/<int:pk>/foods/', views.FoodList.as_view()),
+    # path('catalogs/<int:pk>/foods/<int:pk2>', views.FoodList.as_view()),
     # path('ingredients/', views.IngredientList.as_view()),
     # path('ingredients/<int:pk>/', views.IngredientInfo.as_view()),
     path('login/', views.login),

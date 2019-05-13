3b5ca470edc8ee73c65ca059ad799510f560a592
leila <leiladoskumbay@gmail.com>
2019-05-13 21:31:53 +0600

    a

--- webProject/plantyouBack/api/views/__init__.py
+++
@@ -1,3 +0,0 @@
-from .generics_cbv import CatalogList, CatalogInfo, IngredientInfo, IngredientList, FoodList, FoodInfo
-from .auth import login, logout, UserList, create_user
-from .cbv import Developers, DeveloperInfo

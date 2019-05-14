62d9f704f31322a9ee579a0f85d9180eaaf253f0
aseke7182 <askar_pvl_kz@mail.ru>
2019-05-14 10:45:17 +0600

    asd

--- plantyouBack/plantyouBack/settings.py
+++ plantyouBack/plantyouBack/settings.py
@@ -36,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'api',
+    'main',
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
